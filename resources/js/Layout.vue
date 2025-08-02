4b1582e0d8b1f9a1f2abbaa51fe4bcecc2b507e9
Gab Tamayo <gabotamayo41@gmail.com>
2025-08-02 15:29:29 +0800

    Base App

---
+++ resources/js/Layout.vue
@@ -0,0 +1,17 @@
+<template>
+    <main>
+        <header>
+            <div class="navbar bg-base-100 shadow-sm">
+                <a href="/" class="btn btn-ghost text-xl">daisyUI</a>
+            </div>
+        </header>
+
+        <article class="p-10">
+            <slot />
+        </article>
+    </main>
+</template>
+
+<script setup>
+
+</script>
---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <h1 class="font-bold">Home page</h1>
+    </div>
+</template>
+
+<script setup>
+
+</script>
