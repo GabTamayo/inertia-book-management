2f3b44764064a287ccc2cfb85b0c266c7c0b8fc5
Gab Tamayo <gabotamayo41@gmail.com>
2025-08-04 02:23:13 +0800

    Layout Setup 1

--- resources/js/Layout.vue
+++ resources/js/Layout.vue
@@ -1,17 +1,31 @@
 <template>
     <main>
-        <header>
-            <div class="navbar bg-base-100 shadow-sm">
-                <a href="/" class="btn btn-ghost text-xl">daisyUI</a>
+        <header class="bg-base-100 shadow-sm">
+            <div class="navbar max-w-7xl mx-auto px-2">
+                <div class="navbar-start">
+                    <a class="btn btn-ghost text-xl font-bold">📖bookMNGR</a>
+                </div>
+
+                <div class="navbar-center">
+                    <Nav />
+                </div>
+
+                <div class="navbar-end">
+                    <button class="btn btn-primary">Add A book</button>
+                </div>
             </div>
         </header>
 
-        <article class="p-10">
+        <article class="max-w-7xl mx-auto w-full px-6 py-10">
             <slot />
         </article>
     </main>
 </template>
 
-<script setup>
+<script>
+import Nav from './Shared/Nav.vue';
 
+export default {
+    components: { Nav }
+}
 </script>
---
+++ resources/js/Shared/Nav.vue
@@ -0,0 +1,35 @@
+<template>
+    <nav>
+        <ul class="menu menu-horizontal space-x-2">
+            <li>
+                <Link href="/">Dashboard</Link>
+            </li>
+
+            <li>
+                <Link href="/books">Books</Link>
+            </li>
+
+            <li>
+                <details>
+                    <summary>Settings</summary>
+                    <ul class="bg-base-100 rounded-t-none">
+                        <li>
+                            <Link href="#">Link 1</Link>
+                        </li>
+
+                        <li>
+                            <label class="flex items-center justify-between w-full gap-3">
+                                <span class="text-sm">Theme</span>
+                                <input type="checkbox" value="dark" class="toggle theme-controller" />
+                            </label>
+                        </li>
+                    </ul>
+                </details>
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<script>
+
+</script>
